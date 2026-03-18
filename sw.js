beb8bd799e54ebb6aa84393e814a121c5e2e9737
YuuuuutoOuO <74486861+YuuuuutoOuO@users.noreply.github.com>
2026-03-18 13:10:15 +0800

    Update cache name to brainwallet-v4

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
-const CACHE_NAME = 'brainwallet-v1';
+const CACHE_NAME = 'brainwallet-v4';
 const ASSETS = [
   'index.html',
   'manifest.json',
   'https://cdn.tailwindcss.com'
 ];
 
-// 安裝時抓取檔案
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// 沒網路時從快取抓取
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
